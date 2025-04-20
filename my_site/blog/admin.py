4b0c210f3aa1f20e5f8818daa0029054e48f2dc6
Bohdan <e8653330@gmail.com>
2025-04-20 17:15:13 +0200

    Added db

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Author,Tag,Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields ={"slug":("title",)}
+    list_display = ("title","slug","date",)
+    search_fields = ("title","content",)
+    ordering = ("-date",)
+
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post,PostAdmin)
+
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email_address = models.EmailField( max_length=254)
+
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length = 30)
+
+    def __str__(self) -> str:
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    excerpt = models.CharField(max_length = 50)
+    image_name = models.CharField(max_length = 50)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique = True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author,on_delete = models.SET_NULL,null=True)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.title
